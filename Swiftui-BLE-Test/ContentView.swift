6f4f34a4e01bd8750ea673d3a31c1e83f31684cd
troglodisme <giulio.ammendola@network.rca.ac.uk>
2024-11-07 21:48:55 +0000

    Initial Commit

---
+++ Swiftui-BLE-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftui-BLE-Test
+//
+//  Created by Giulio on 07/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftui-BLE-Test/Swiftui_BLE_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_BLE_TestApp.swift
+//  Swiftui-BLE-Test
+//
+//  Created by Giulio on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_BLE_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
